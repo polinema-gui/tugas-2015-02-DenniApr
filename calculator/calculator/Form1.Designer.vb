842d7ebb54d1f3fb9d80f7a39a24941c83ce116b
Deni Aprianto <denniapr12@gmail.com>
2015-10-09 19:05:26 +0700

    calculator

---
+++ calculator/calculator/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.no1 = New System.Windows.Forms.Button()
+        Me.no2 = New System.Windows.Forms.Button()
+        Me.no3 = New System.Windows.Forms.Button()
+        Me.no4 = New System.Windows.Forms.Button()
+        Me.no5 = New System.Windows.Forms.Button()
+        Me.no6 = New System.Windows.Forms.Button()
+        Me.no7 = New System.Windows.Forms.Button()
+        Me.no8 = New System.Windows.Forms.Button()
+        Me.no9 = New System.Windows.Forms.Button()
+        Me.no0 = New System.Windows.Forms.Button()
+        Me.smdgn = New System.Windows.Forms.Button()
+        Me.clear = New System.Windows.Forms.Button()
+        Me.krg = New System.Windows.Forms.Button()
+        Me.tbh = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'no1
+        '
+        Me.no1.Location = New System.Drawing.Point(12, 48)
+        Me.no1.Name = "no1"
+        Me.no1.Size = New System.Drawing.Size(41, 37)
+        Me.no1.TabIndex = 0
+        Me.no1.Text = "1"
+        Me.no1.UseVisualStyleBackColor = True
+        '
+        'no2
+        '
+        Me.no2.Location = New System.Drawing.Point(60, 48)
+        Me.no2.Name = "no2"
+        Me.no2.Size = New System.Drawing.Size(42, 40)
+        Me.no2.TabIndex = 1
+        Me.no2.Text = "2"
+        Me.no2.UseVisualStyleBackColor = True
+        '
+        'no3
+        '
+        Me.no3.Location = New System.Drawing.Point(107, 48)
+        Me.no3.Name = "no3"
+        Me.no3.Size = New System.Drawing.Size(42, 40)
+        Me.no3.TabIndex = 2
+        Me.no3.Text = "3"
+        Me.no3.UseVisualStyleBackColor = True
+        '
+        'no4
+        '
+        Me.no4.Location = New System.Drawing.Point(12, 91)
+        Me.no4.Name = "no4"
+        Me.no4.Size = New System.Drawing.Size(42, 40)
+        Me.no4.TabIndex = 3
+        Me.no4.Text = "4"
+        Me.no4.UseVisualStyleBackColor = True
+        '
+        'no5
+        '
+        Me.no5.Location = New System.Drawing.Point(60, 94)
+        Me.no5.Name = "no5"
+        Me.no5.Size = New System.Drawing.Size(42, 40)
+        Me.no5.TabIndex = 4
+        Me.no5.Text = "5"
+        Me.no5.UseVisualStyleBackColor = True
+        '
+        'no6
+        '
+        Me.no6.Location = New System.Drawing.Point(107, 94)
+        Me.no6.Name = "no6"
+        Me.no6.Size = New System.Drawing.Size(42, 40)
+        Me.no6.TabIndex = 5
+        Me.no6.Text = "6"
+        Me.no6.UseVisualStyleBackColor = True
+        '
+        'no7
+        '
+        Me.no7.Location = New System.Drawing.Point(12, 140)
+        Me.no7.Name = "no7"
+        Me.no7.Size = New System.Drawing.Size(42, 40)
+        Me.no7.TabIndex = 6
+        Me.no7.Text = "7"
+        Me.no7.UseVisualStyleBackColor = True
+        '
+        'no8
+        '
+        Me.no8.Location = New System.Drawing.Point(59, 140)
+        Me.no8.Name = "no8"
+        Me.no8.Size = New System.Drawing.Size(42, 40)
+        Me.no8.TabIndex = 7
+        Me.no8.Text = "8"
+        Me.no8.UseVisualStyleBackColor = True
+        '
+        'no9
+        '
+        Me.no9.Location = New System.Drawing.Point(107, 140)
+        Me.no9.Name = "no9"
+        Me.no9.Size = New System.Drawing.Size(42, 40)
+        Me.no9.TabIndex = 8
+        Me.no9.Text = "9"
+        Me.no9.UseVisualStyleBackColor = True
+        '
+        'no0
+        '
+        Me.no0.Location = New System.Drawing.Point(12, 186)
+        Me.no0.Name = "no0"
+        Me.no0.Size = New System.Drawing.Size(42, 40)
+        Me.no0.TabIndex = 9
+        Me.no0.Text = "0"
+        Me.no0.UseVisualStyleBackColor = True
+        '
+        'smdgn
+        '
+        Me.smdgn.Location = New System.Drawing.Point(60, 186)
+        Me.smdgn.Name = "smdgn"
+        Me.smdgn.Size = New System.Drawing.Size(89, 40)
+        Me.smdgn.TabIndex = 10
+        Me.smdgn.Text = "="
+        Me.smdgn.UseVisualStyleBackColor = True
+        '
+        'clear
+        '
+        Me.clear.Location = New System.Drawing.Point(155, 48)
+        Me.clear.Name = "clear"
+        Me.clear.Size = New System.Drawing.Size(42, 86)
+        Me.clear.TabIndex = 11
+        Me.clear.Text = "clear"
+        Me.clear.UseVisualStyleBackColor = True
+        '
+        'krg
+        '
+        Me.krg.Location = New System.Drawing.Point(154, 143)
+        Me.krg.Name = "krg"
+        Me.krg.Size = New System.Drawing.Size(42, 40)
+        Me.krg.TabIndex = 12
+        Me.krg.Text = "-"
+        Me.krg.UseVisualStyleBackColor = True
+        '
+        'tbh
+        '
+        Me.tbh.Location = New System.Drawing.Point(155, 186)
+        Me.tbh.Name = "tbh"
+        Me.tbh.Size = New System.Drawing.Size(42, 40)
+        Me.tbh.TabIndex = 13
+        Me.tbh.Text = "+"
+        Me.tbh.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.TextBox1.Size = New System.Drawing.Size(185, 20)
+        Me.TextBox1.TabIndex = 14
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(209, 238)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.tbh)
+        Me.Controls.Add(Me.krg)
+        Me.Controls.Add(Me.clear)
+        Me.Controls.Add(Me.smdgn)
+        Me.Controls.Add(Me.no0)
+        Me.Controls.Add(Me.no9)
+        Me.Controls.Add(Me.no8)
+        Me.Controls.Add(Me.no7)
+        Me.Controls.Add(Me.no6)
+        Me.Controls.Add(Me.no5)
+        Me.Controls.Add(Me.no4)
+        Me.Controls.Add(Me.no3)
+        Me.Controls.Add(Me.no2)
+        Me.Controls.Add(Me.no1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents no1 As System.Windows.Forms.Button
+    Friend WithEvents no2 As System.Windows.Forms.Button
+    Friend WithEvents no3 As System.Windows.Forms.Button
+    Friend WithEvents no4 As System.Windows.Forms.Button
+    Friend WithEvents no5 As System.Windows.Forms.Button
+    Friend WithEvents no6 As System.Windows.Forms.Button
+    Friend WithEvents no7 As System.Windows.Forms.Button
+    Friend WithEvents no8 As System.Windows.Forms.Button
+    Friend WithEvents no9 As System.Windows.Forms.Button
+    Friend WithEvents no0 As System.Windows.Forms.Button
+    Friend WithEvents smdgn As System.Windows.Forms.Button
+    Friend WithEvents clear As System.Windows.Forms.Button
+    Friend WithEvents krg As System.Windows.Forms.Button
+    Friend WithEvents tbh As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+
+End Class
---
+++ calculator/calculator/Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class Form1
+
+    Private Sub no1_Click(sender As Object, e As EventArgs) Handles no1.Click
+        TextBox1.Text = TextBox1.Text + "1"
+    End Sub
+
+    Private Sub no2_Click(sender As Object, e As EventArgs) Handles no2.Click
+        TextBox1.Text = TextBox1.Text + "2"
+    End Sub
+
+    Private Sub no3_Click(sender As Object, e As EventArgs) Handles no3.Click
+        TextBox1.Text = TextBox1.Text + "3"
+    End Sub
+
+    Private Sub no4_Click(sender As Object, e As EventArgs) Handles no4.Click
+        TextBox1.Text = TextBox1.Text + "4"
+    End Sub
+
+    Private Sub no5_Click(sender As Object, e As EventArgs) Handles no5.Click
+        TextBox1.Text = TextBox1.Text + "5"
+    End Sub
+
+    Private Sub no6_Click(sender As Object, e As EventArgs) Handles no6.Click
+        TextBox1.Text = TextBox1.Text + "6"
+    End Sub
+
+    Private Sub no7_Click(sender As Object, e As EventArgs) Handles no7.Click
+        TextBox1.Text = TextBox1.Text + "7"
+    End Sub
+
+    Private Sub no8_Click(sender As Object, e As EventArgs) Handles no8.Click
+        TextBox1.Text = TextBox1.Text + "8"
+    End Sub
+
+    Private Sub no9_Click(sender As Object, e As EventArgs) Handles no9.Click
+        TextBox1.Text = TextBox1.Text + "9"
+    End Sub
+
+    Private Sub no0_Click(sender As Object, e As EventArgs) Handles no0.Click
+        TextBox1.Text = TextBox1.Text + "0"
+    End Sub
+
+    Dim h1 As String
+    Dim nil As Integer
+    Private Sub smdgn_Click(sender As Object, e As EventArgs) Handles smdgn.Click
+        If h1 = "+" Then
+            TextBox1.Text = nil + Me.TextBox1.Text
+        End If
+        If h1 = "-" Then
+            TextBox1.Text = nil - Me.TextBox1.Text
+        End If
+
+    End Sub
+    Private Sub krg_Click(sender As Object, e As EventArgs) Handles krg.Click
+        nil = Val(TextBox1.Text)
+        TextBox1.Text = Me.TextBox1.Text & " - "
+        Me.TextBox1.Text = ""
+        h1 = "-"
+
+    End Sub
+
+    Private Sub tbh_Click(sender As Object, e As EventArgs) Handles tbh.Click
+        nil = Val(TextBox1.Text)
+        TextBox1.Text = Me.TextBox1.Text & " + "
+        Me.TextBox1.Text = ""
+        h1 = "+"
+    End Sub
+
+    Private Sub clear_Click(sender As Object, e As EventArgs) Handles clear.Click
+        TextBox1.Text = ""
+        Me.TextBox1.Text = ""
+
+
+
+
+    End Sub
+End Class
